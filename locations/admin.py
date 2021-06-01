37e287a7a0e6c9ec88a6ce0ba98d3a10efd78822
macbotxxx <assanamamichael@gmail.com>
2021-06-01 11:44:25 +0100

    locations app admin registered

--- locations/admin.py
+++ locations/admin.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
+from .models import Country, State, City
 
 
-
+@admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('name' , 'iso2', 'created_date', 'modified_date')
     list_display_links = ('name' , 'iso2', 'created_date', 'modified_date')
 
 
+
+@admin.register(State)
 class StateAdmin(admin.ModelAdmin):
     list_display = ('name' , 'iso2','country_code', 'created_date', 'modified_date')
     list_display_links = ('name' , 'iso2','country_code', 'created_date', 'modified_date')
 
 
+@admin.register(City)
 class CityAdmin(admin.ModelAdmin):
     list_display = ('name' , 'country_code','state_code', 'created_date', 'modified_date')
     list_display_links = ('name' , 'country_code','state_code', 'created_date', 'modified_date')
